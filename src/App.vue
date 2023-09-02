f64f988d5156fc07925de084139fe8d23cea00d8
franciscocoya <kikocoya@gmail.com>
2023-09-02 11:27:19 +0200

    feat: project skeleton

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BaseTextArea from './components/BaseTextArea.vue';
+</script>
+
+<template>
+  <main>
+    <BaseTextArea />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/BaseTextArea.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Comprobar unfollowers</h1>
+                <label for="json-input">Selecciona el fichero JSON "followers_1.json" y el fichero "following.json". Ambos ficheros son obligatorios para realizar la comprobación.</label>
+                <input type="file" name="json-input" id="json-input" accept="">
+                <button>Subir</button>
+            </div>
+        </div>
+    </div>
+</template>
